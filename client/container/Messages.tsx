5a6f80f4b61b8771a27f281862c3f148ce4d8112
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-23 13:30:27 +0530

    styles + message box

--- client/container/Messages.tsx
+++ client/container/Messages.tsx
@@ -1,10 +1,12 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useSockets } from "../context/socket.context";
 import EVENTS from "../config/events";
+import styles from "../styles/Message.module.css";
 
 function MessageContainer() {
   const { socket, messages, roomId, username, setMessages } = useSockets();
   const newMessageRef = useRef(null);
+  const messageEndRef = useRef(null);
 
   function handleSendMessage() {
     const message = newMessageRef.current.value;
@@ -12,7 +14,6 @@ function MessageContainer() {
       return;
     }
     socket.emit(EVENTS.CLIENT.SEND_ROOM_MESSAGE, { roomId, message, username });
-console.log(messages,"MMMM")
     const date = new Date();
     setMessages([
       ...messages,
@@ -25,24 +26,40 @@ console.log(messages,"MMMM")
     newMessageRef.current.value = "";
   }
 
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behaviour: "smooth" });
+  }, [messages]);
+
   if (!roomId) {
     return <div />;
   }
 
   return (
-    <div>
-      {messages &&
-        messages.map(({ message }, index) => {
-          return <p key={index}>{message}</p>;
-        })}
+    <div className={styles.wrapper}>
+      <div className={styles.messageList}>
+        {messages &&
+          messages.map(({ message, username, time }, index) => {
+            return (
+              <div key={index} className={styles.message}>
+                <div key={index} className={styles.messageInner}>
+                  <span className={styles.messageSender}>
+                    {username} - {time}
+                  </span>
+                  <span className={styles.messageBody}>{message}</span>
+                </div>
+              </div>
+            );
+          })}
+        <div ref={messageEndRef} />
 
-      <div>
-        <textarea
-          rows={1}
-          placeholder="Tell Us What you are thinkinh"
-          ref={newMessageRef}
-        />
-        <button onClick={handleSendMessage}>Send</button>
+        <div className={styles.messageBox}>
+          <textarea
+            rows={1}
+            placeholder="Tell Us What you are thinkinh"
+            ref={newMessageRef}
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
       </div>
     </div>
   );
